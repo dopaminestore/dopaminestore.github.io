f291cbf275b08bb8b8b550fc11b709e870e41547
Sai-Adarsh <saiadarshsivakumar@gmail.com>
2020-10-19 01:51:12 +0530

    Updates

---
+++ precache-manifest.2e7c1f3755cc5f002d28b2e379e233f0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac3ccc94dcfc5cb0e697676ea8f70a55",
+    "url": "/dopaminestore.github.io/index.html"
+  },
+  {
+    "revision": "7c7fc4e2ab33b64607de",
+    "url": "/dopaminestore.github.io/static/css/main.80f77e77.chunk.css"
+  },
+  {
+    "revision": "8641a293e0758f2250fe",
+    "url": "/dopaminestore.github.io/static/js/2.9355d199.chunk.js"
+  },
+  {
+    "revision": "9cf9d893b5c51de0aa6dd9e07be64537",
+    "url": "/dopaminestore.github.io/static/js/2.9355d199.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7fc4e2ab33b64607de",
+    "url": "/dopaminestore.github.io/static/js/main.23651ed1.chunk.js"
+  },
+  {
+    "revision": "2b06cac1fc5ef1028305",
+    "url": "/dopaminestore.github.io/static/js/runtime-main.d9378747.js"
+  },
+  {
+    "revision": "4cdacb8f89d588d69e8570edcbe49507",
+    "url": "/dopaminestore.github.io/static/media/Poppins-SemiBold.4cdacb8f.ttf"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/dopaminestore.github.io/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "c5d8962846415ddf2a106713f228ada1",
+    "url": "/dopaminestore.github.io/static/media/rocket.c5d89628.gif"
+  }
+]);
